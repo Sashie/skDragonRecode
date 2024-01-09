5f2c8071c67101e354e00ed258e0204d54664219
Sashie <ud13n0w@gmail.com>
2024-01-09 14:16:05 -0330

    Changing colors to skript/bukkit colors

---
+++ src/main/java/me/sashie/skdragon/effects/targets/Arc.java
@@ -0,0 +1,81 @@
+package me.sashie.skdragon.effects.targets;
+
+import org.bukkit.Particle;
+import org.bukkit.util.Vector;
+
+import me.sashie.skdragon.effects.EffectProperty;
+import me.sashie.skdragon.effects.TargetEffect;
+import me.sashie.skdragon.effects.properties.DensityProperty;
+import me.sashie.skdragon.effects.properties.ExtraProperty;
+import me.sashie.skdragon.effects.properties.IDensity;
+import me.sashie.skdragon.effects.properties.IExtra;
+import me.sashie.skdragon.particles.ColoredParticle;
+import me.sashie.skdragon.particles.ParticleBuilder;
+import me.sashie.skdragon.util.DynamicLocation;
+import me.sashie.skdragon.util.EffectUtils;
+import me.sashie.skdragon.util.VectorUtils;
+
+
+/**
+ * Created by Sashie on 12/12/2016.
+ */
+
+public class Arc extends TargetEffect implements IDensity, IExtra {
+
+	private DensityProperty densityProperty;
+	private ExtraProperty extraProperty;
+
+	public Arc() {
+		densityProperty = new DensityProperty();
+		extraProperty = new ExtraProperty();
+
+		this.getExtraProperty().initValue(1.5f, 1.5f, 1.5f);
+		this.getDensityProperty().initDensity(40);
+
+	}
+
+	@Override
+	public void update(DynamicLocation location, DynamicLocation target, float step) {
+        Vector link = target.toVector().subtract(location.toVector());
+        float length = (float) link.length();
+        float pitch = (float) (this.getExtraProperty().getValue(2) * this.getExtraProperty().getValue(1) / Math.pow(length, 2));//pitchMultiplier default = 4
+        for (int i = 0; i < this.getDensityProperty().getDensity(1); i++) {
+            Vector v = link.clone().normalize().multiply((float) length * i / this.getDensityProperty().getDensity(1));
+            float x = ((float) i / this.getDensityProperty().getDensity(1)) * length - length / 2;
+            float y = (float) (-pitch * Math.pow(x, 2) + this.getExtraProperty().getValue(1));
+            //TODO test \/
+            Vector v2 = new Vector(0, y, 0);
+            v2 = VectorUtils.rotateAroundAxisX(v2, this.getExtraProperty().getValue(3));
+            location.add(v).add(v2);//.add(0, y, 0);
+            this.getParticleBuilder(1).sendParticles(location, this.getPlayers());
+            location.subtract(0, y, 0).subtract(v);
+        }
+        //TODO make it rotate more
+	}
+
+	@Override
+	public void onUnregister() {
+
+	}
+
+	@Override
+	public EffectProperty[] acceptProperties() {	//TODO
+		return EffectUtils.array(EffectProperty.DENSITY, EffectProperty.EXTRA);
+	}
+
+	@Override
+	public ParticleBuilder<?>[] defaultParticles() {
+		return new ParticleBuilder<?>[] { new ColoredParticle(Particle.REDSTONE) };
+	}
+
+	@Override
+	public ExtraProperty getExtraProperty() {
+		return extraProperty;
+	}
+
+	@Override
+	public DensityProperty getDensityProperty() {
+		return densityProperty;
+	}
+
+}
---
+++ src/main/java/me/sashie/skdragon/effects/targets/TargetLightning.java
@@ -0,0 +1,148 @@
+package me.sashie.skdragon.effects.targets;
+
+import me.sashie.skdragon.particles.ParticleBuilder;
+import me.sashie.skdragon.util.pool.ObjectPoolManager;
+import org.bukkit.Particle;
+import org.bukkit.util.Vector;
+
+import me.sashie.skdragon.effects.EffectProperty;
+import me.sashie.skdragon.effects.TargetEffect;
+import me.sashie.skdragon.effects.properties.DensityProperty;
+import me.sashie.skdragon.effects.properties.ExtraProperty;
+import me.sashie.skdragon.effects.properties.IDensity;
+import me.sashie.skdragon.effects.properties.IExtra;
+import me.sashie.skdragon.effects.properties.IRadius;
+import me.sashie.skdragon.effects.properties.IStyle;
+import me.sashie.skdragon.effects.properties.RadiusProperty;
+import me.sashie.skdragon.effects.properties.StyleProperty;
+import me.sashie.skdragon.particles.ColoredParticle;
+import me.sashie.skdragon.util.DynamicLocation;
+import me.sashie.skdragon.util.EffectUtils;
+import me.sashie.skdragon.util.RandomUtils;
+
+/**
+ * Created by Sashie on 12/12/2016.
+ */
+
+public class TargetLightning extends TargetEffect implements IRadius, IDensity, IStyle, IExtra {
+	
+	private RadiusProperty radiusProperty;
+	private StyleProperty styleProperty;
+	private ExtraProperty extraProperty;
+	private DensityProperty densityProperty;
+	private DynamicLocation loc;
+
+	public TargetLightning() {
+		densityProperty = new DensityProperty();
+		radiusProperty = new RadiusProperty();
+		styleProperty = new StyleProperty();
+		extraProperty = new ExtraProperty();
+		
+		this.getExtraProperty().initValue(0.5f);
+		this.getRadiusProperty().initRadius(0.5f);
+		this.getDensityProperty().initDensity(40);
+	}
+
+	@Override
+	public void update(DynamicLocation location, DynamicLocation target, float step) {
+		electricity(location, target, this.getRadiusProperty().getRadius(1), this.getDensityProperty().getDensity(1));
+	}
+	
+	public void electricity(DynamicLocation startLoc, DynamicLocation endLoc, float maxOffset, int density) {
+		double currentX = startLoc.getX();
+		double currentY = startLoc.getY();
+		double currentZ = startLoc.getZ();
+		double lastX = startLoc.getX();
+		double lastY = startLoc.getY();
+		double lastZ = startLoc.getZ();
+		
+		//int segments = (int) (startLoc.distance(endLoc) / distanceBetweenPoints) + 1;
+		//int segments = (int) (startLoc.distanceSquared(endLoc) / distanceBetweenPoints) + 1;
+		double distance = startLoc.distanceSquared(endLoc);
+		int segments = 1;
+		if (this.getStyleProperty().getStyle() == 1)
+			segments = (int) (distance / this.getExtraProperty().getValue(1)) + 1;
+		else if (this.getStyleProperty().getStyle() == 2)
+			segments = (int) (distance / RandomUtils.randomRangeDouble(distance / this.getExtraProperty().getValue(1), distance / 2)) + 1;
+		else
+			segments = this.getExtraProperty().getValue(1).intValue();
+
+		//System.out.println(segments / density);
+		//TODO make a better equation for total length to segment length ratio
+		int dps = density / segments;
+		if (dps <= 1)
+			dps = 1;
+		
+		for (int i = 0; i < segments; i++) {
+			currentX = ((endLoc.getX() - startLoc.getX()) / segments) * i + startLoc.getX();
+			currentY = ((endLoc.getY() - startLoc.getY()) / segments) * i + startLoc.getY();
+			currentZ = ((endLoc.getZ() - startLoc.getZ()) / segments) * i + startLoc.getZ();
+			
+			// offsetting
+			currentX += maxOffset * RandomUtils.random(1f) * RandomUtils.randomSign();
+			currentY += maxOffset * RandomUtils.random(1f) * RandomUtils.randomSign();
+			currentZ += maxOffset * RandomUtils.random(1f) * RandomUtils.randomSign();
+			
+			Vector start = new Vector(lastX, lastY, lastZ);
+			Vector end = new Vector(currentX, currentY, currentZ);
+			
+			line(start, end, dps);
+			
+			lastX = currentX;
+			lastY = currentY;
+			lastZ = currentZ;
+		}
+		
+		line(new Vector(currentX, currentY, currentZ), new Vector(endLoc.getX(), endLoc.getY(), endLoc.getZ()), dps);
+	}
+
+	private void line(Vector start, Vector end, int density) {
+		Vector link = end.subtract(start);
+		float length = (float) link.length();
+		link.normalize();
+		float ratio = length / density;
+		Vector v = link.multiply(ratio);
+		//DynamicLocation loc = new DynamicLocation(start.subtract(v).toLocation(this.getLocations()[0].getWorld()));
+		loc = ObjectPoolManager.getDynamicLocationPool().acquire(start.subtract(v).toLocation(this.getLocations()[0].getWorld()));
+
+		for (int i = 0; i < density; i++) {
+			loc.add(v);
+		    this.getParticleBuilder(1).sendParticles(loc, this.getPlayers());
+		}
+	}
+
+	@Override
+	public void onUnregister() {
+		ObjectPoolManager.getDynamicLocationPool().release(loc);
+	}
+
+	@Override
+	public EffectProperty[] acceptProperties() {
+		return EffectUtils.array(EffectProperty.STYLE, EffectProperty.RADIUS, EffectProperty.DENSITY, EffectProperty.EXTRA);
+	}
+
+	@Override
+	public ParticleBuilder<?>[] defaultParticles() {
+		return new ParticleBuilder<?>[] { new ColoredParticle(Particle.REDSTONE) };
+	}
+
+	@Override
+	public ExtraProperty getExtraProperty() {
+		return extraProperty;
+	}
+
+	@Override
+	public RadiusProperty getRadiusProperty() {
+		return radiusProperty;
+	}
+
+	@Override
+	public DensityProperty getDensityProperty() {
+		return densityProperty;
+	}
+
+	@Override
+	public StyleProperty getStyleProperty() {
+		return styleProperty;
+	}
+}
